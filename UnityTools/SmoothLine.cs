bb87e1dfdad95fc0810a46fbdc1587d1bdf6e934
Kaya Tacer <ktacer@outlook.com>
2022-09-14 14:22:25 -0700

    updated smoothline

--- UnityTools/SmoothLine.cs
+++ UnityTools/SmoothLine.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class SmoothLine : MonoBehaviour
 {
+    public Transform target;
     public int vertexCount = 20;
     Vector3[] vertices;
     public float lerp = 100;
@@ -15,12 +16,12 @@ public class SmoothLine : MonoBehaviour
     }
 
     private void Update() {
-        Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 targetPos = target ? target.position : Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         for(int i = vertexCount - 1; i > 0; i--){
             vertices[i] = Vector3.Lerp(vertices[i], vertices[i - 1], lerp * Time.deltaTime);
         }
-        vertices[0] = (Vector3)target;
+        vertices[0] = (Vector3)targetPos;
         GetComponent<LineRenderer>().SetPositions(vertices);
     }
 }
