a64d22c5068dc9f255c433ccb60d5876e54581fb
Kaya Tacer <ktacer@outlook.com>
2022-09-14 14:32:17 -0700

    added unity project

--- Assets/UnityTools/SmoothLine.cs
+++ Assets/UnityTools/SmoothLine.cs
@@ -8,7 +8,7 @@ public class SmoothLine : MonoBehaviour
     public Transform target;
     public int vertexCount = 20;
     Vector3[] vertices;
-    public float lerp = 100;
+    public float speed = 10;
 
     private void Start() {
         vertices = new Vector3[vertexCount];
@@ -19,7 +19,7 @@ public class SmoothLine : MonoBehaviour
         Vector2 targetPos = target ? target.position : Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         for(int i = vertexCount - 1; i > 0; i--){
-            vertices[i] = Vector3.Lerp(vertices[i], vertices[i - 1], lerp * Time.deltaTime);
+            vertices[i] = Vector3.Lerp(vertices[i], vertices[i - 1], speed * 10 * Time.deltaTime);
         }
         vertices[0] = (Vector3)targetPos;
         GetComponent<LineRenderer>().SetPositions(vertices);
